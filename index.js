3ed8e13b683645850d15736c9a5019f5d043afe9
Ryan Chu <ryaneof@gmail.com>
2015-09-16 16:14:09 +0800

    temporary fix, using pickadate with browserify, using jquery from window.$

---
+++ index.js
@@ -0,0 +1,26 @@
+// bundle
+
+(function (root, factory, undef) {
+  if (typeof exports === "object") {
+    // CommonJS
+    module.exports = exports = factory(
+      require('./lib/picker'),
+      require('./lib/picker.date'),
+      require('./lib/picker.time')
+    );
+  } else if (typeof define === "function" && define.amd) {
+    // AMD
+    define([
+      './lib/picker',
+      './lib/picker.date',
+      './lib/picker.time'
+    ], factory);
+  } else {
+    // Global (browser)
+    factory(root.Picker);
+  }
+}(this, function (Picker) {
+
+  return Picker;
+
+}));
